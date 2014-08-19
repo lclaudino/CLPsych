515bf82326b90cb0e207bc5373c64fd07c8945da
claudino <claudino@Leonardo-Claudinos-MacBook-Pro-2.local>
2014-08-19 18:30:15 -0400

    Adding external submodules

---
+++ external/download_others.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Download Mallet
+wget http://mallet.cs.umass.edu/dist/mallet-2.0.7.tar.gz
+tar -zxvf mallet-2.0.7.tar.gz
+rm mallet-2.0.7.tar.gz
+
+# Download the Tomcat version of ITM
+wget http://www.umiacs.umd.edu/~claudino/summer-2014/itm-release-install.tar.gz
+tar -zxvf itm-release-install.tar.gz
+rm itm-release-install.tar.gz
+
+
+
