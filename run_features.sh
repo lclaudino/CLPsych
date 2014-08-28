2dba641a0ea20d99ff0bd3e8b2ec439985675c4c
claudino <claudino@Leonardo-Claudinos-MacBook-Pro-2.local>
2014-08-28 14:10:11 -0400

    Updated run_lda.sh and added run_features.sh

--- run_features.sh
+++ run_features.sh
@@ -10,6 +10,7 @@ INPUTDIR=$BASEDIR/input/
 OUTPUTDIR=/tmp/clpsych/output/ # <----- change here
 EXTERNAL=$BASEDIR/external/
 DATAFILE=$INPUTDIR/wmdtweets_2014.tsv.median # <----- change here
+LIWC_PICKLE=$INPUTDIR/licw_arabic_dict.pkl
 
 mkdir -p $OUTPUTDIR/feat-files
 mkdir -p $OUTPUTDIR/topic_features
@@ -23,28 +24,9 @@ export PYTHONPATH=/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.
 #########################
 # Create features
 
-LIWC_PICKLE=$INPUTDIR/licw_arabic_dict.pkl
-
 $PYTHON $BASEDIR/src/features/prepare_features.py \
 --input_tsv_regexp=$DATAFILE \
 --feat_folder=$OUTPUTDIR/feat-files/ \
 --liwc_dict_filename=$LIWC_PICKLE
 
-##########################
-# Train models for k folds
-K=10
-NUM_TOPICS=5
-NUM_ITE=1000
-ALPHA=100
-SEED=11901
-MODEL=MalletLDA
-PATH_TO_MODEL_BIN=$EXTERNAL/mallet-2.0.7/bin/
-
-$PYTHON $BASEDIR/src/experiments/train_topics.py \
---out_folder=$OUTPUTDIR/topic-feats/ \
---feature_pkl_file_regexp=$OUTPUTDIR/feat-files/$( basename $DATAFILE ).sent.pkl \
---bin_path=$PATH_TO_MODEL_BIN \
---k=$K \
---topic_model_args=num_topics=$NUM_TOPICS,num_ite=$NUM_ITE,alpha=$ALPHA,seed=$SEED \
---topic_model_name=$MODEL
-
+ls -la $OUTPUTDIR/feat-files/
